1ddffcb4eca57a8a422f5c572cb679c96545d48f
dheerajdhall <dhall.d@husky.neu.edu>
2019-11-26 00:55:53 -0500

    Deleted old files

--- restapp/src/main/java/com/basic/api/restapp/repo/ObjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.basic.api.restapp.repo;
-
-import com.basic.api.restapp.domain.ObjectModel;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface ObjectRepository extends CrudRepository<ObjectModel, Long> {
-
-}
